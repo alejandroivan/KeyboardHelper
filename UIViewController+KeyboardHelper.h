492588e1ccf5e2a649f3ab105198727f3fc0e7d4
Alejandro Iván <alejandro@ratiboo.cl>
2017-06-06 15:40:16 -0400

    Added some methods for easy management of scrolling the current UITextField above the keyboard.

--- UIViewController+KeyboardHelper.h
+++ UIViewController+KeyboardHelper.h
@@ -2,19 +2,33 @@
 //  UIViewController+KeyboardHelper.h
 //
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 
 @interface UIViewController (KeyboardHelper)
 
-@property (strong, nonatomic) UITapGestureRecognizer *tapGestureRecognizer;
+@property (strong, nonatomic) UITapGestureRecognizer *tapGestureRecognizerKH;
+@property (strong, nonatomic) NSMutableArray *tapGestureRecognizerViewsKH;
+@property (weak, nonatomic) UIScrollView *scrollViewKH;
+
+
+- (void)addGestureRecognizerToView:(UIView *)theView;
+- (void)removeGestureRecognizerFromView:(UIView *)theView;
+- (void)removeGestureRecognizerFromAllViews;
 
 - (void)configureTapGestureRecognizer;
+- (void)configureTapGestureRecognizerWithView:(UIView *)theView;
+
 - (void)dismissKeyboard;
 
 - (UIView *)findFirstResponder;
 - (UIView *)findFirstResponderFromViewController:(UIViewController *)viewController;
 - (UIView *)findFirstResponderFromView:(UIView *)rootView;
 
+
 - (void)changeFirstResponderToView:(UIView *)nextResponder;
 
+// Get notified and scroll a scroll view properly when started editing a form
+- (void)registerForKeyboardNotifications;
+- (void)registerForKeyboardNotificationsWithScrollView:(UIScrollView *)theScrollView;
+- (void)deregisterForKeyboardNotifications;
 @end
--- UIViewController+KeyboardHelper.m
+++ UIViewController+KeyboardHelper.m
@@ -5,85 +5,122 @@
 #import <objc/runtime.h>
 #import "UIViewController+KeyboardHelper.h"
 
-static void *TapGestureRecognizerPropertyKey = &TapGestureRecognizerPropertyKey;
+static void *TapGestureRecognizerKHPropertyKey      = &TapGestureRecognizerKHPropertyKey;
+static void *TapGestureRecognizerViewsKHPropertyKey = &TapGestureRecognizerViewsKHPropertyKey;
+static void *ScrollViewKHPropertyKey                = &ScrollViewKHPropertyKey;
+
+
+
 
 @implementation UIViewController (KeyboardHelper)
 
-
-
-
 // Getter/setter for tap gesture recognizer
-- (void)setTapGestureRecognizer:(UITapGestureRecognizer *)tgr
-{
-    objc_setAssociatedObject( self, TapGestureRecognizerPropertyKey, tgr, OBJC_ASSOCIATION_RETAIN_NONATOMIC );
+- (void)setTapGestureRecognizerKH:(UITapGestureRecognizer *)tgr {
+    objc_setAssociatedObject( self, TapGestureRecognizerKHPropertyKey, tgr, OBJC_ASSOCIATION_RETAIN_NONATOMIC );
 }
 
-- (UITapGestureRecognizer *)tapGestureRecognizer
-{
-    return objc_getAssociatedObject( self, TapGestureRecognizerPropertyKey );
+- (UITapGestureRecognizer *)tapGestureRecognizerKH {
+    return objc_getAssociatedObject( self, TapGestureRecognizerKHPropertyKey );
 }
 
+// Getter/setter for tap gesture recognizer views array
+- (void)setTapGestureRecognizerViewsKH:(UITapGestureRecognizer *)tgr {
+    objc_setAssociatedObject( self, TapGestureRecognizerViewsKHPropertyKey, tgr, OBJC_ASSOCIATION_RETAIN_NONATOMIC );
+}
+
+- (UITapGestureRecognizer *)tapGestureRecognizerViewsKH {
+    return objc_getAssociatedObject( self, TapGestureRecognizerViewsKHPropertyKey );
+}
+
+// Getter/setter for the scroll view outlet
+- (void)setScrollViewKH:(UIScrollView *)scrollViewKH {
+    objc_setAssociatedObject( self, ScrollViewKHPropertyKey, scrollViewKH, OBJC_ASSOCIATION_RETAIN_NONATOMIC );
+}
+
+- (UIScrollView *)scrollViewKH {
+    return objc_getAssociatedObject( self, ScrollViewKHPropertyKey );
+}
 
 
 
 #pragma mark - Tap Gesture Recognizer setup
-- (void)configureTapGestureRecognizer
-{
-    self.tapGestureRecognizer                           = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapOnMainView:)];
-    self.tapGestureRecognizer.cancelsTouchesInView      = NO;
-    self.tapGestureRecognizer.numberOfTapsRequired      = 1;
-    self.tapGestureRecognizer.numberOfTouchesRequired   = 1;
-    
-    [self.view addGestureRecognizer:self.tapGestureRecognizer];
+- (void)configureTapGestureRecognizer {
+    [self configureTapGestureRecognizerWithView:self.view];
 }
 
+- (void)configureTapGestureRecognizerWithView:(UIView *)theView {
+    self.tapGestureRecognizerKH                           = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapOnMainView:)];
+    self.tapGestureRecognizerKH.cancelsTouchesInView      = NO;
+    self.tapGestureRecognizerKH.numberOfTapsRequired      = 1;
+    self.tapGestureRecognizerKH.numberOfTouchesRequired   = 1;
+    
+    [self addGestureRecognizerToView:theView];
+}
 
-- (void)handleTapOnMainView:(UITapGestureRecognizer *)sender // Override on view controller if needed
-{
+- (void)addGestureRecognizerToView:(UIView *)theView {
+    if ( [self.tapGestureRecognizerViewsKH containsObject:theView] ) {
+        return;
+    }
+    
+    [self.tapGestureRecognizerViewsKH addObject:theView];
+    [theView addGestureRecognizer:self.tapGestureRecognizerKH];
+}
+
+- (void)removeGestureRecognizerFromView:(UIView *)theView {
+    [theView removeGestureRecognizer:self.tapGestureRecognizerKH];
+    [self.tapGestureRecognizerViewsKH removeObject:theView];
+}
+
+- (void)removeGestureRecognizerFromAllViews {
+    for ( UIView *theView in self.tapGestureRecognizerViewsKH ) {
+        [self removeGestureRecognizerFromView:theView];
+    }
+}
+
+- (void)handleTapOnMainView:(UITapGestureRecognizer *)sender { // Override on view controller if needed
     [self dismissKeyboard];
 }
 
 
+
+
+
 #pragma mark - Dismiss Keyboard
-- (void)dismissKeyboard
-{
+- (void)dismissKeyboard {
     [self changeFirstResponderToView:nil];
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField // Override on view controller
-{
+- (BOOL)textFieldShouldReturn:(UITextField *)textField { // Override on view controller
     [self dismissKeyboard];
     return YES;
 }
 
 
 #pragma mark - Find First Responder
-- (UIView *)findFirstResponder
-{
+- (UIView *)findFirstResponder {
     return [self findFirstResponderFromViewController:self];
 }
 
 
 
-- (UIView *)findFirstResponderFromViewController:(UIViewController *)viewController
-{
+- (UIView *)findFirstResponderFromViewController:(UIViewController *)viewController {
     return [self findFirstResponderFromView:viewController.view];
 }
 
 
 
-- (UIView *)findFirstResponderFromView:(UIView *)rootView
-{
+- (UIView *)findFirstResponderFromView:(UIView *)rootView {
     UIView *firstResponder = [rootView isFirstResponder] ? rootView : nil;
     
-    if ( ! firstResponder )
-        for (UIView *subview in rootView.subviews )
-        {
+    if ( ! firstResponder ) {
+        for (UIView *subview in rootView.subviews ) {
             firstResponder = [self findFirstResponderFromView:subview];
             
-            if ( firstResponder )
+            if ( firstResponder ) {
                 break;
+            }
         }
+    }
     
     return firstResponder;
 }
@@ -92,17 +129,123 @@ static void *TapGestureRecognizerPropertyKey = &TapGestureRecognizerPropertyKey;
 
 
 #pragma mark Change First Responder
-- (void)changeFirstResponderToView:(UIView *)nextResponder
-{
+- (void)changeFirstResponderToView:(UIView *)nextResponder {
     UIView *firstResponder = [self findFirstResponder];
     
-    if ( firstResponder == nextResponder )
+    if ( firstResponder == nextResponder ) {
         return;
-    
+    }
     
     [firstResponder resignFirstResponder];
     [nextResponder becomeFirstResponder];
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pragma mark Show inputs when keyboard appears
+// Call this method somewhere in your view controller setup code.
+
+- (void)registerForKeyboardNotifications {
+    
+    if ( ! self.scrollViewKH ) {
+#ifdef DEBUG
+#if DEBUG == 1
+        NSLog(@"<KeyboardEvent> registerForKeyboardNotifications should only be called after setting the UIScrollView (setScrollViewKH: method)");
+#endif
+#endif
+        return;
+    }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)registerForKeyboardNotificationsWithScrollView:(UIScrollView *)theScrollView {
+    [self setScrollViewKH:theScrollView];
+    [self registerForKeyboardNotifications];
+}
+
+- (void)deregisterForKeyboardNotifications {
+    [self setScrollViewKH:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardDidShowNotification
+                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+// Called when the UIKeyboardDidShowNotification is sent.
+- (void)keyboardWasShown:(NSNotification*)aNotification {
+    if ( ! self.scrollViewKH ) {
+#ifdef DEBUG
+#if DEBUG == 1
+        NSLog(@"<KeyboardEvent> Se debe definir la UIScrollView utilizando el método setScrollViewKH:");
+#endif
+#endif
+        return;
+    }
+    
+    UIScrollView *scrollView            = (UIScrollView *) self.scrollViewKH;
+    NSDictionary* info                  = [aNotification userInfo];
+    CGSize kbSize                       = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    UIView *activeView                  = [self findFirstResponderFromView:self.view];
+    
+    UIEdgeInsets contentInsets          = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    scrollView.contentInset             = contentInsets;
+    scrollView.scrollIndicatorInsets    = contentInsets;
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your app might not need or want this behavior.
+    CGRect aRect        = self.view.frame;
+    aRect.size.height   -= kbSize.height;
+    
+    CGPoint originPoint = activeView.frame.origin;
+    CGPoint endPoint    = activeView.frame.origin;
+    endPoint.x          += activeView.frame.size.width;
+    endPoint.y          += activeView.frame.size.height;
+    
+    if ( ! CGRectContainsPoint(aRect, originPoint) || ! CGRectContainsPoint(aRect, endPoint) ) {
+        [scrollView scrollRectToVisible:activeView.frame
+                               animated:YES];
+    }
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification {
+    if ( ! self.scrollViewKH ) {
+#ifdef DEBUG
+#if DEBUG == 1
+        NSLog(@"<KeyboardEvent> Se debe definir la UIScrollView utilizando el método setScrollViewKH:");
+#endif
+#endif
+        return;
+    }
+    
+    UIScrollView *scrollView            = (UIScrollView *) self.scrollViewKH;
+    UIEdgeInsets contentInsets          = UIEdgeInsetsZero;
+    
+    scrollView.contentInset             = contentInsets;
+    scrollView.scrollIndicatorInsets    = contentInsets;
+}
 
 @end
